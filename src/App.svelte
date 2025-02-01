c855ac57937fe0c9d3832daca4a0f20d59d4d1dc
odinellefsen <odin@ellefsen.fo>
2025-02-01 00:55:17 +0000

    removed boilerplate and readme

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main class="max-w-[1280px] m-auto p-8 text-center">
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#ff3e00aa]" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1 class="text-5xl leading-tight">Vite + Svelte</h1>
-
   <div class="p-8">
     <Counter />
   </div>
